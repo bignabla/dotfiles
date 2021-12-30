c0a4d808b5403e86c52bf6b777e7d3b1efa082a1
bignabla <michelepalma.nabla@gmail.com>
2021-12-30 11:49:08 +0000

    Add zsh config

--- install.sh
+++ install.sh
@@ -18,6 +18,10 @@ ln -s "$HOME/dotfiles/i3" "$HOME/.config"
 #######
 # zsh #
 #######
-#mkdir -p "$HOME/.config/zsh"
-#ln -sf "$HOME/dotfiles/zsh/.zshenv" "$HOME"
-#ln -sf "$HOME/dotfiles/zsh/.zshrc" "$HOME/.config/zsh"
+mkdir -p "$HOME/.config/zsh"
+ln -sf "$HOME/dotfiles/zsh/.zshenv" "$HOME"
+ln -sf "$HOME/dotfiles/zsh/.zshrc" "$HOME/.config/zsh"
+ln -sf "$HOME/dotfiles/zsh/aliases" "$HOME/.config/zsh/aliases"
+rm -rf "$HOME/.config/zsh/external"
+ln -sf "$HOME/dotfiles/zsh/external" "$HOME/.config/zsh"
+
